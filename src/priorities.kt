a5ca3f5d41ca25d5806b10355808d3433e44f24e
benfarice <imzoughene@outlook.com>
2018-08-22 21:27:14 +0100

    Priorities rules

---
+++ src/priorities.kt
@@ -0,0 +1,16 @@
+/*
+Operations rules | priorities rules
+1- ()
+2- ^
+3- *,/
+4- +,-
+5- =
+ */
+fun main(args:Array<String>) {
+    var n1:Int=34
+    var n2:Int=65
+    var n3:Int=29
+    var result:Int?
+    result=n1+n2*n3+56
+    print("the result is $result")
+}
