7f784200e1ef7a85b01c963730b831e4a5296753
benfarice <imzoughene@outlook.com>
2018-08-22 18:45:07 +0100

    Null Safety

---
+++ src/NullSafety.kt
@@ -0,0 +1,15 @@
+fun main(args:Array<String>) {
+
+    var nom:String
+    nom="imzoughene"
+    println("nom : "+nom)
+
+
+    var var_allow_null:String?
+    var_allow_null=null
+    println("my var : "+var_allow_null)
+
+    var allow_null:String?
+    allow_null = "youssef"
+    println("do not allow null in this statement if null throw an exception : "+allow_null!!)
+}
