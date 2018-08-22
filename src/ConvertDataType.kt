162e61196c7fdaee79086cadb1006be8b14889c6
benfarice <imzoughene@outlook.com>
2018-08-22 19:28:10 +0100

    Convert Data Type

---
+++ src/ConvertDataType.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>) {
+
+    var n1:Int = 10
+    var n2:Int?
+    var n2Str:String="12"
+    n2=n2Str.toInt()
+    println("Sum : "+(n1+n2))
+
+    var xpi:Double = 3.14
+    println("xpi : "+xpi)
+    var intPi:Int=xpi.toInt()
+    println("intPi : "+intPi)
+}
