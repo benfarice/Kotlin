1af3faadbaf86c00c946d0935d2f250bba8ca0c2
benfarice <imzoughene@outlook.com>
2018-08-22 16:22:00 +0100

    Data Types

---
+++ src/DataTypes.kt
@@ -0,0 +1,19 @@
+fun main(args:Array<String>) {
+
+    var name = "Imzoughene Youssef"
+
+    var job:String = "Developer"
+
+    var age = 25
+
+    var yearOfBirth:Int = 1993
+
+    var Dep:String?
+
+    Dep = "Software Engineering"
+
+    var pi:Float = 3.14f
+
+
+    println("name : "+name)
+}
