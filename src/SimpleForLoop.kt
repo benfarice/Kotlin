ae3b2e9d87ede2879df9a4cc0a810e9d28e0a459
benfarice <imzoughene@outlook.com>
2018-09-05 19:29:56 +0100

    Simple For Loop

---
+++ src/SimpleForLoop.kt
@@ -0,0 +1,8 @@
+fun main(args:Array<String>){
+    for(item in 1..10){
+        //block of code
+        println("hello Imzoughene")
+        println("Count :  $item")
+    }
+    println("Loop Done")
+}
