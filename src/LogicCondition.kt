2993fa0a4856dda199419d2e85b0d4330db7b9d4
benfarice <imzoughene@outlook.com>
2018-09-05 15:43:52 +0100

    Logic Condition

---
+++ src/LogicCondition.kt
@@ -0,0 +1,16 @@
+fun main(args:Array<String>){
+    println(3<9)
+    println(8<2)
+    println(0==7)
+    println(5!=8)
+    println(4<8 && 89>3)
+
+    var i:Int=45
+    println(i>5 && i<40)
+
+    var t:Int = 66
+    println(t==76 || t<245)
+
+    var is_married=true
+    println(!is_married)
+}
