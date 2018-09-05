43aafd5ade40593ee23e9d4dbe305ae6a2422a46
benfarice <imzoughene@outlook.com>
2018-09-05 20:55:01 +0100

    Nested For Loop

---
+++ src/NestedForLoop.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>){
+    for(count1 in 1..10){
+
+        println("Count A :  $count1")
+
+        for(count2 in 1..7){
+
+            println("Count B :  $count2")
+        }
+        println("Nested Loop Done")
+    }
+    println("Loop Done")
+}
