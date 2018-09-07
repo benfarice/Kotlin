6d831b231695dd3dc66360778a456d2ac51e362a
benfarice <imzoughene@outlook.com>
2018-09-07 15:14:21 +0100

    While Loop && Do While Loop

---
+++ src/DoWhileLoop.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>){
+
+    var count = 1
+    do{
+        //block of code
+        println("hello Imzoughene")
+        println("Count :  $count")
+        count++
+    }while(count<= 10)
+    println("Loop Done")
+}
---
+++ src/WhileLoop.kt
@@ -0,0 +1,11 @@
+fun main(args:Array<String>){
+
+    var count = 1
+    while(count<= 10){
+        //block of code
+        println("hello Imzoughene")
+        println("Count :  $count")
+        count++
+    }
+    println("Loop Done")
+}
